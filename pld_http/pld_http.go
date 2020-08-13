bafbb0aae4a41e8ee29c43d90692914ebf16f278
michaelzx <mail@zhanxiao.org>
2020-08-13 10:24:37 +0800

    增加文件下载函数

---
+++ pld_http/pld_http.go
@@ -0,0 +1,76 @@
+package pld_http
+
+import (
+	"errors"
+	"github.com/michaelzx/pld/pld_fs"
+	"io"
+	"net/http"
+	"os"
+)
+
+// 如果文件有存在，则先删除
+func deleteIfExists(filePath string) error {
+	if pld_fs.Exists(filePath) {
+		return os.Remove(filePath)
+	}
+	return nil
+}
+
+func DownloadFile(url string, localPath string) error {
+	// ******************************************************
+	// 下载的临时文件名称
+	// ******************************************************
+	tmpFilePath := localPath + ".download" // 没下载成功之前，用download后缀作为临时文件
+	// 如果临时文件已存在，则删除
+	err := deleteIfExists(tmpFilePath)
+	if err != nil {
+		return err
+	}
+	// ******************************************************
+	// 创建一个http client
+	// ******************************************************
+	client := new(http.Client)
+	// client.Timeout = time.Second * 60 //设置超时时间
+	resp, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	// ******************************************************
+	// 读取服务器返回的文件大小
+	// ******************************************************
+	// fSize, err = strconv.ParseInt(resp.Header.Get("Content-Length"), 10, 32)
+	// if err != nil {
+	// 	return err
+	// }
+	// ******************************************************
+	// 创建并写入临时文件
+	// ******************************************************
+	if resp.Body == nil {
+		return errors.New("body is null")
+	}
+	defer resp.Body.Close()
+
+	outFile, err := os.Create(tmpFilePath)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	// 将响应流和文件流对接起来
+	_, err = io.Copy(outFile, resp.Body)
+	if err != nil {
+		return err
+	}
+	err = outFile.Close()
+	if err != nil {
+		return err
+	}
+	// ******************************************************
+	// 用临时文件替换正式文件
+	// ******************************************************
+	err = os.Rename(tmpFilePath, localPath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ pld_http/pld_http_test.go
@@ -0,0 +1,13 @@
+package pld_http
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDownloadFile(t *testing.T) {
+	err := DownloadFile("https://www.baidu.com/img/PCfb_5bf082d29588c07f842ccde3f97243ea.png", "./test.png")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
