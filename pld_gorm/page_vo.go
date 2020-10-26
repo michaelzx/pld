c647f46bcf4e569a3a4ab3e257f3dc762eef5e94
michaelzx <mail@zhanxiao.org>
2020-10-26 22:15:19 +0800

    优化分页对象中的最后 一页判断

--- pld_gorm/page_vo.go
+++ pld_gorm/page_vo.go
@@ -69,10 +69,10 @@ func (p *PageVO) get(db *gorm.DB, sqlTpl string, params interface{}) error {
 		p.IsFirstPage = true
 		p.IsLastPage = true
 	} else {
-		switch p.PageNum {
-		case 1:
+		if p.PageNum == 1 {
 			p.IsFirstPage = true
-		case p.PageTotal:
+		}
+		if p.PageNum == p.PageTotal {
 			p.IsLastPage = true
 		}
 	}
